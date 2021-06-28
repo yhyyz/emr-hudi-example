c5feb5dbe851cd88c668f521d536124d67f9da95
Pan <chaopan@08f8bc6ad936.ant.amazon.com>
2021-06-29 00:15:32 +0800

    fix future

--- script/sysbanch_test.sh
+++ script/sysbanch_test.sh
@@ -1,20 +1,20 @@
 # 创建表
 sysbench creates.lua \
-    --mysql-user=admin --mysql-password=panchao0129#$  \
-    --mysql-host=news-test.cszxtewro2cv.ap-southeast-1.rds.amazonaws.com \
+    --mysql-user=admin --mysql-password=******  \
+    --mysql-host=*****.ap-southeast-1.rds.amazonaws.com \
     --mysql-db=cdc_test_db --report-interval=1 \
     --events=1 run
 
 # 插入数据
 sysbench insert.lua \
-    --mysql-user=admin --mysql-password=panchao0129#$  \
-    --mysql-host=news-test.cszxtewro2cv.ap-southeast-1.rds.amazonaws.com \
+    --mysql-user=admin --mysql-password=******  \
+    --mysql-host=*****.ap-southeast-1.rds.amazonaws.com \
     --mysql-db=cdc_test_db --report-interval=1 \
     --events=1000000 --time=0 --threads=10  --skip_trx=true run
 
 # 更新数据
 sysbench update.lua \
-    --mysql-user=admin --mysql-password=panchao0129#$  \
-    --mysql-host=news-test.cszxtewro2cv.ap-southeast-1.rds.amazonaws.com \
+    --mysql-user=admin --mysql-password=******  \
+    --mysql-host=*****.ap-southeast-1.rds.amazonaws.com \
     --mysql-db=cdc_test_db --report-interval=1 \
     --events=10000 --time=0 --threads=10 --skip_trx=true --update_id_min=1 --update_id_max=200000 run
